fe06677939992620bbf4f5e40b7f4c3f9e8871ad
unknown <sidneyos@icloud.com>
2022-03-19 13:16:22 -0300

    IncurtadorURL

---
+++ IncurtadorURL/src/config/Constants.ts
@@ -0,0 +1,6 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION: 'mongodb+srv://sidiney:<password>@incurtadorurl.6yua7.mongodb.net/incurtadorURL?retryWrites=true&w=majority'
+
+
+}
